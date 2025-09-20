f261cb589c900bbdf828c6c07cc9192b935d5ca0
Uttam Kumar Dey <uttam.dey@comviva.com>
2025-09-20 20:55:42 +0530

    Generic method and Moonshot has been added.

--- SwiftUI_All/Resources/Astronaut.swift
+++ SwiftUI_All/Resources/Astronaut.swift
@@ -6,16 +6,9 @@
 //
 
 import Foundation
-
-
-struct Astronaut: Codable, Identifiable {
-    var id: String
-    var name: String
-    var description: String
-}
-
+import SwiftUI
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -23,8 +16,13 @@ extension Bundle {
             fatalError("Failed to load \(file) from bundle.")
         }
         let decoder = JSONDecoder()
+        let dateFormatter = DateFormatter()
+        let locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.locale = locale
+        dateFormatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
         do {
-            return try decoder.decode([String: Astronaut].self, from: data)
+            return try decoder.decode(T.self, from: data)
         } catch DecodingError.keyNotFound(let key, let context) {
             fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' – \(context.debugDescription)")
         } catch DecodingError.typeMismatch(_, let context) {
@@ -37,4 +35,45 @@ extension Bundle {
             fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
         }
     }
+}
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+    
 }
--- SwiftUI_All/Screens/Moonshot.swift
+++ SwiftUI_All/Screens/Moonshot.swift
@@ -9,51 +9,240 @@ import SwiftUI
 
 
 struct Moonshot: View {
+    @State private var showingGrid = false
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
-        Text("Astronauts \(astronauts.count)")
-            .font(.title)
+        NavigationStack {
+            Group {
+                if showingGrid {
+                    GridLayout(astronauts: astronauts, missions: missions)
+                } else {
+                    ListLayout(astronauts: astronauts, missions: missions)
+                }
+            }
+            .toolbar(content: {
+                Button("Some Button", systemImage: showingGrid ? "circle.grid.3x3.circle" : "list.bullet.circle") {
+                    showingGrid.toggle()
+                }
+            })
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+struct ListLayout: View {
+    let rows = [
+        GridItem(.flexible())
+    ]
+    var astronauts: [String: Astronaut]
+    var missions: [Mission]
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: rows) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronaut: astronauts)
+                        } label: {
+                            HStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.6))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            }
+                            .padding([.horizontal, .bottom])
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct GridLayout: View {
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    var astronauts: [String: Astronaut]
+    var missions: [Mission]
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronaut: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.6))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            }
+                            .padding([.horizontal, .bottom])
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}
+struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                Text("Launch Date: \(mission.formattedLaunchDate)")
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 1)
+                    .foregroundStyle(.gray)
+                    .padding(.vertical)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    Text(mission.description)
+                    
+                    Rectangle()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .foregroundStyle(.gray)
+                        .padding(.vertical)
+                    
+                    Text("Crew")
+                        .font(.title2.bold())
+                        .foregroundStyle(.white)
+                        .padding(.bottom, 5)
+                    
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(crew, id: \.role) { crew in
+                        NavigationLink {
+                            AstronautView(astronaut: crew.astronaut)
+                        } label: {
+                            HStack {
+                                Image(crew.astronaut.id)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 104, height: 72)
+                                    .clipShape(.circle)
+                                    .overlay {
+                                        Circle().strokeBorder(.white, lineWidth: 1)
+                                    }
+                                VStack(alignment: .leading) {
+                                    Text(crew.astronaut.name)
+                                        .foregroundStyle(.white)
+                                        .font(.caption)
+                                    Text(crew.role)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.6))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+        
+    }
+    init(mission: Mission, astronaut: [String: Astronaut]) {
+        self.mission = mission
+        self.crew = mission.crew.map({ member in
+            if let astronaut = astronaut[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        })
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                Text(astronaut.description)
+                    .font(.headline)
+                    .padding()
+            }
+            
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
 
 
 struct MoonshotApp: View {
     var body: some View {
-        ScrollView(.horizontal) { // for Horizonatal scrolling we need to have HStack inside ScrollView
+        ScrollView(.horizontal) { // For Horizonatal scrolling we need to have HStack inside ScrollView
             LazyHStack(spacing: 10) { // LazyHStack takes the all available space while HStack takes only the required space
                 ForEach(0..<100) { item in
                     Text("Item \(item)")
@@ -68,14 +257,14 @@ struct MoonshotApp: View {
 struct NavigationView: View {
     var body: some View {
         NavigationStack {
-                List {
-                    ForEach(0..<100) { item in
-                        NavigationLink("Next \(item) Screen") {
-                            Text("Screen \(item)")
-                                .font(.title)
-                        }
+            List {
+                ForEach(0..<100) { item in
+                    NavigationLink("Next \(item) Screen") {
+                        Text("Screen \(item)")
+                            .font(.title)
                     }
                 }
+            }
         }
     }
 }
@@ -108,9 +297,6 @@ struct JsonParser: View {
             if let user = try? decoder.decode(User.self, from: data) {
                 print(user.address.street)
             }
-            
-
-            // more code to come
         }
     }
 }
